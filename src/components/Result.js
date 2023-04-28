b3120a3b94d646377384605dcdc7fec4c9bc27cd
Mayank <mayankbansal125@gmail.com>
2023-04-28 18:01:46 +0530

    result and result table component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom"
+import "../styles/Result.css";
+import ResultTable from "./ResultTable";
 
 export default function result() {
+  function onRestart(){
+    console.log("On Restart");
+  }
   return (
     <div className='container'>
-      
+      <h1 className='title text-light'>Quiz Application</h1>
+      <div className='result flex-center'>
+        <div className='flex'>
+          <span>Username</span>
+          <span className='bold'>Benaam</span>
+        </div>
+        <div className='flex'>
+          <span>Total Quiz Points:</span>
+          <span className='bold'>50</span>
+        </div>
+        <div className='flex'>
+          <span>Total Attempts:</span>
+          <span className='bold'>03</span>
+        </div>
+        <div className='flex'>
+          <span>Total Earn Points</span>
+          <span className='bold'>30</span>
+        </div>
+        <div className='flex'>
+          <span>Quiz Result</span>
+          <span className='bold'>Passed</span>
+        </div>
+      </div>
+      <div className='start'>
+        <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+      </div>
+      <div className='container'>
+        <ResultTable />
+      </div>
     </div>
   )
 }
---
+++ src/components/ResultTable.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function ResultTable() {
+  return (
+    <div>
+        <table>
+            <thead className='table-header'>
+                <tr className='table-row'>
+                    <td>Name</td>
+                    <td>Attemps</td>
+                    <td>Earn Points</td>
+                    <td>Result</td>
+                </tr>
+            </thead>
+            <tbody className='table-body'>
+            <tr className='table-row'>
+                    <td>Benaam</td>
+                    <td>03</td>
+                    <td>30</td>
+                    <td>Passed</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
